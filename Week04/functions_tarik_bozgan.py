e5707873d0c57b10fc89492aaf213f036215bab3
protagonist9 <bozgan.tarik@gmail.com>
2025-12-06 21:04:09 +0300

    10th commit to fix bugs

--- Week04/functions_tarik_bozgan.py
+++ Week04/functions_tarik_bozgan.py
@@ -1,6 +1,6 @@
 import sys
 
-custom_power = lambda x, /, e=1: x**e
+custom_power = lambda x=0, /, e=1: x**e
 
 def custom_equation(x: int = 0, y: int = 0, /, a: int = 1, b: int = 1, *, c: int = 1) -> float:
     """
@@ -16,7 +16,7 @@ def custom_equation(x: int = 0, y: int = 0, /, a: int = 1, b: int = 1, *, c: int
     """
     return (x**a + y**b) / c
 
-def fn_w_counter() -> tuple:
+def fn_w_counter() -> tuple[int, dict[str, int]]:
     if not hasattr(fn_w_counter, 'count'):
         fn_w_counter.count = 0
         fn_w_counter.callers = {}
