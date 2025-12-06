3dfc44683bbb4d7f0a7d529d445bc1b43419700f
protagonist9 <bozgan.tarik@gmail.com>
2025-12-06 20:42:32 +0300

    Bugs got fixed

--- Week04/decorators_tarik_bozgan.py
+++ Week04/decorators_tarik_bozgan.py
@@ -1,15 +1,3 @@
-custom_power = lambda x, e=1: x ** e
-
-def custom_equation(x, y, a=1, b=1, *, c=1):
-    return (x ** a + y ** b) / c
-
-_calls = {}
-
-def fn_w_counter():
-    caller = __import__('inspect').currentframe().f_back.f_code.co_name
-    _calls[caller] = _calls.get(caller, 0) + 1
-    return _calls[caller], dict(_calls)
-
 def performance(f):
     def w(*a, **k):
         import time, tracemalloc
--- Week04/functions_tarik_bozgan.py
+++ Week04/functions_tarik_bozgan.py
@@ -1,12 +1,11 @@
-def custom_power(x, e=1):
-    return x ** e
+custom_power = lambda x, e=1: x ** e
 
 def custom_equation(x, y, a=1, b=1, *, c=1):
     return (x ** a + y ** b) / c
 
-call_counts = {}
+_calls = {}
 
 def fn_w_counter():
-    caller_name = __import__('inspect').currentframe().f_back.f_code.co_name
-    call_counts[caller_name] = call_counts.get(caller_name, 0) + 1
-    return (call_counts[caller_name], {k: v for k, v in call_counts.items()})
+    caller = __import__('inspect').currentframe().f_back.f_code.co_name
+    _calls[caller] = _calls.get(caller, 0) + 1
+    return _calls[caller], dict(_calls)
